575fb12939d18125d27a7714ea8dace0e0f7429f
Pavan Poudel <poudelpavan@gmail.com>
2020-12-09 17:17:21 -0500

    parameters add

--- src/mpid/ch4/netmod/ofi/ofi_send.h
+++ src/mpid/ch4/netmod/ofi/ofi_send.h
@@ -390,7 +390,7 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_NM_mpi_isend(const void *buf, MPI_Aint count,
 
     if (!MPIDI_OFI_ENABLE_TAGGED) {
         mpi_errno =
-            MPIDIG_mpi_isend(buf, count, datatype, rank, tag, comm, context_offset, addr, request);
+            MPIDIG_mpi_isend(buf, count, datatype, rank, tag, comm, context_offset, addr, vni_src, vni_dst, request);
     } else {
         int vni_src, vni_dst;
         MPIDI_OFI_SEND_VNIS(vni_src, vni_dst);  /* defined just above */
