a7ecfe6721f7b2d443995228a32039b9c764da9c
Pavan Poudel <poudelpavan@gmail.com>
2020-12-11 10:39:20 -0500

    add vni parameters to ofi_recv.h

--- src/mpid/ch4/netmod/ofi/ofi_recv.h
+++ src/mpid/ch4/netmod/ofi/ofi_recv.h
@@ -303,13 +303,14 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_NM_mpi_irecv(void *buf,
 
     /* For anysource recv, we may be called while holding the vci lock of shm request (to
      * prevent shm progress). Therefore, recursive locking is allowed here */
+    
+    int vni_src, vni_dst;
+    MPIDI_OFI_RECV_VNIS(vni_src, vni_dst);
     if (!MPIDI_OFI_ENABLE_TAGGED) {
         mpi_errno =
-            MPIDIG_mpi_irecv(buf, count, datatype, rank, tag, comm, context_offset, request, 0,
+            MPIDIG_mpi_irecv(buf, count, datatype, rank, tag, comm, context_offset, vni_src, vni_dst, request, 0,
                              partner);
     } else {
-        int vni_src, vni_dst;
-        MPIDI_OFI_RECV_VNIS(vni_src, vni_dst);
         MPID_THREAD_CS_ENTER_REC_VCI(MPIDI_VCI(vni_dst).lock);
         mpi_errno = MPIDI_OFI_do_irecv(buf, count, datatype, rank, tag, comm,
                                        context_offset, addr, vni_src, vni_dst, request,
