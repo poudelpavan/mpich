9235679da23a71a1fe6b73cc2abd1d385f14d4d7
Pavan Poudel <poudelpavan@gmail.com>
2020-12-14 18:15:06 -0500

    define an inline-function and a macro to get vni_src and vni_dst in netmod_fallback

--- src/mpid/ch4/netmod/include/netmod_am_fallback_send.h
+++ src/mpid/ch4/netmod/include/netmod_am_fallback_send.h
@@ -6,6 +6,28 @@
 #ifndef NETMOD_AM_FALLBACK_SEND_H_INCLUDED
 #define NETMOD_AM_FALLBACK_SEND_H_INCLUDED
 
+/* vni mapping */
+/* NOTE: concerned by the modulo? If we restrict num_vnis to power of 2,
+ * we may get away with bit mask */
+MPL_STATIC_INLINE_PREFIX int MPIDI_NM_get_vni(int flag, MPIR_Comm * comm_ptr,
+                                               int src_rank, int dst_rank, int tag)
+{
+    return MPIDI_get_vci(flag, comm_ptr, src_rank, dst_rank, tag) % MPIDI_global.n_vcis;
+}
+
+/* Common macro used by all MPIDI_POSIX_mpi_send routines to compute vnis */
+#define MPIDI_NM_SEND_VNIS(vni_src_, vni_dst_) \
+    do { \
+        if (*request != NULL) { \
+            /* workq path */ \
+            vni_src_ = 0; \
+            vni_dst_ = 0; \
+        } else { \
+            vni_src_ = MPIDI_NM_get_vni(SRC_VCI_FROM_SENDER, comm, comm->rank, rank, tag); \
+            vni_dst_ = MPIDI_NM_get_vni(DST_VCI_FROM_SENDER, comm, comm->rank, rank, tag); \
+        } \
+    } while (0)
+
 MPL_STATIC_INLINE_PREFIX int MPIDI_NM_mpi_isend(const void *buf,
                                                 MPI_Aint count,
                                                 MPI_Datatype datatype,
@@ -15,6 +37,7 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_NM_mpi_isend(const void *buf,
                                                 MPIDI_av_entry_t * addr, MPIR_Request ** request)
 {
     int vni_src = 0, vni_dst = 0;
+    MPIDI_NM_SEND_VNIS(vni_src_, vni_dst_);
     return MPIDIG_mpi_isend(buf, count, datatype, rank, tag, comm, context_offset, addr, vni_src, vni_dst, request);
 }
 
