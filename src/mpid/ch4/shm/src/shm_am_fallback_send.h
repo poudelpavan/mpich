16b36f2892e3acd4c6a7e4359d9da7432a564d04
Pavan Poudel <poudelpavan@gmail.com>
2020-12-14 18:02:12 -0500

    define an inline-function and a macro to get vni_src and vni_dst in shm_fallback

--- src/mpid/ch4/shm/src/shm_am_fallback_send.h
+++ src/mpid/ch4/shm/src/shm_am_fallback_send.h
@@ -6,6 +6,31 @@
 #ifndef SHM_AM_FALLBACK_SEND_H_INCLUDED
 #define SHM_AM_FALLBACK_SEND_H_INCLUDED
 
+#include "mpidig_am.h"
+#include "ch4_impl.h"
+
+/* vni mapping */
+/* NOTE: concerned by the modulo? If we restrict num_vnis to power of 2,
+ * we may get away with bit mask */
+MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_get_vni(int flag, MPIR_Comm * comm_ptr,
+                                               int src_rank, int dst_rank, int tag)
+{
+    return MPIDI_get_vci(flag, comm_ptr, src_rank, dst_rank, tag) % MPIDI_global.n_vcis;
+}
+
+/* Common macro used by all MPIDI_POSIX_mpi_send routines to compute vnis */
+#define MPIDI_SHM_SEND_VNIS(vni_src_, vni_dst_) \
+    do { \
+        if (*request != NULL) { \
+            /* workq path */ \
+            vni_src_ = 0; \
+            vni_dst_ = 0; \
+        } else { \
+            vni_src_ = MPIDI_SHM_get_vni(SRC_VCI_FROM_SENDER, comm, comm->rank, rank, tag); \
+            vni_dst_ = MPIDI_SHM_get_vni(DST_VCI_FROM_SENDER, comm, comm->rank, rank, tag); \
+        } \
+    } while (0)
+
 MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_mpi_isend(const void *buf,
                                                  MPI_Aint count,
                                                  MPI_Datatype datatype,
@@ -15,6 +40,7 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_mpi_isend(const void *buf,
                                                  MPIDI_av_entry_t * addr, MPIR_Request ** request)
 {
     int vni_src = 0, vni_dst = 0;
+    MPIDI_SHM_SEND_VNIS(vni_src, vni_dst);
     return MPIDIG_mpi_isend(buf, count, datatype, rank, tag, comm, context_offset, addr, vni_src, vni_dst, request);
 }
 
