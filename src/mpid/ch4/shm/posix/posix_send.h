97e5ead7c216cb888fb4dc6f2575c090e89525df
Pavan Poudel <poudelpavan@gmail.com>
2020-12-14 09:58:34 -0500

    define a macro to get vni_src and vni_dst inside posix_mpi_send()

--- src/mpid/ch4/shm/posix/posix_send.h
+++ src/mpid/ch4/shm/posix/posix_send.h
@@ -16,12 +16,26 @@
 
 #include "posix_impl.h"
 
+/* Common macro used by all MPIDI_POSIX_mpi_send routines to compute vnis */
+#define MPIDI_POSIX_SEND_VNIS(vni_src_, vni_dst_) \
+    do { \
+        if (*request != NULL) { \
+            /* workq path */ \
+            vni_src_ = 0; \
+            vni_dst_ = 0; \
+        } else { \
+            vni_src_ = MPIDI_OFI_get_vni(SRC_VCI_FROM_SENDER, comm, comm->rank, rank, tag); \
+            vni_dst_ = MPIDI_OFI_get_vni(DST_VCI_FROM_SENDER, comm, comm->rank, rank, tag); \
+        } \
+    } while (0)
+
 MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_mpi_isend(const void *buf, MPI_Aint count,
                                                    MPI_Datatype datatype, int rank, int tag,
                                                    MPIR_Comm * comm, int context_offset,
                                                    MPIDI_av_entry_t * addr, MPIR_Request ** request)
 {
     int vni_src = 0, vni_dst = 0;
+    MPIDI_POSIX_SEND_VNIS(vni_src, vni_dst);
     return MPIDIG_mpi_isend(buf, count, datatype, rank, tag, comm, context_offset, addr, vni_src, vni_dst, request);
 }
 
